94aa47fa256f58dd1124ef6d7f33bcfc026a0cda
tnt <tntzxzttt@gmail.com>
2026-05-30 10:35:39 +0900

    feat(fish): set up fish to use Starship (#4)

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,3 +7,6 @@ fish_add_path --path /opt/homebrew/bin
 fish_add_path --path /opt/homebrew/opt/mysql-client/bin
 fish_add_path --path /opt/homebrew/opt/asdf/libexec/bin
 fish_add_path --path /Applications/Ghostty.app/Contents/MacOS
+
+# Enable Starship
+starship init fish | source
