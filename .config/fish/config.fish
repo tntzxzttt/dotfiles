25f917fbdbe11a6b5cea56d8c4a55f7453a33919
tnt <tntzxzttt@gmail.com>
2026-05-30 10:41:00 +0900

    feat(fish): configure direnv (#5)

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,3 +10,7 @@ fish_add_path --path /Applications/Ghostty.app/Contents/MacOS
 
 # Enable Starship
 starship init fish | source
+
+# WARNINGS: This direnv setup must be placed at the end of this file.
+# Refer to https://direnv.net/docs/hook.html#fish.
+direnv hook fish | source
